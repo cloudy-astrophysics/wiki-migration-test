a8ac7a5081b1c82b394f05a3f6c8b270c83e6188
William Henney <will@henney.org>
2019-10-17 13:22:23 -0500

    Add my own function to try and fix the WikiLinks
    This works most of the time, but still needs polishing.  For instance,
    I should check that word is surrounded by either whitespace or begin,
    end of line

--- test-trac2down.py
+++ test-trac2down.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from pathlib import Path
 sys.path.append("../../../Source/tracboat/src/tracboat")
 import trac2down
@@ -10,11 +11,39 @@ try:
 except (IndexError, ValueError, TypeError):
     sys.exit(f"Usage: {sys.argv[0]} INPUT_PATH SAVE_PATH")
 
-# Loop over all files in the input folder
+# Get the names so that we can convert links to gollum format
+# "CamelCaseName" -> "[[CamelCaseName]]"
+wikinames = [p.name for p in input_path.glob("*")]
+
+
+def fixup_wikilinks(text, targets):
+    """Add double [[...]] around wikilinks (given as `targets`)
+
+    Most prosaic method possible - simply loop over the explicit list
+    of targets.  Tries to be careful in avoiding code blocks, but
+    will still be fooled by inline code
+    """
+    lines = []
+    is_code_block = False
+    for line in text.split('\n'):
+        # not blockquote?
+        if not line.startswith('    '):
+            if line.startswith("````"):
+                is_code_block = not is_code_block
+            if not is_code_block:
+                for target in targets:
+                    line = line.replace(target, f"[[{target}]]")
+            # line = re.sub(r'\!(([A-Z][a-z0-9]+){2,})', r'[[\1]]', line)
+        lines.append(line)
+    return "\n".join(lines)
+
+
+# Process all the files in the input folder
 for p in input_path.glob("*"):
     # Open file from the dump of the trac database and convert it
     with open(p) as f:
         text = trac2down.convert(f.read(), ".")
+        text = fixup_wikilinks(text, wikinames)
     # Save the converted file with a ".md" extension to output folder
     # The 3rd, 4th, 5th arguments to this function are unused
     trac2down.save_file(text, p.name, None, None, None, save_path)
